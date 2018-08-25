886e8cd47ee461485b62101cb37a7e550460fb7a
Justine <jjtroyke@gmail.com>
2018-08-25 14:29:03 -0600

    Created ursl within app and foods

--- tracker/tests.py
+++ tracker/tests.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from rest_framework.test import APIClient
 from django.test import TestCase
 from tracker.models import Food
 
@@ -17,10 +18,12 @@ class Test_FoodTestCase(TestCase):
         Food.objects.create(name="tacos", calories=325)
         Food.objects.create(name="rice", calories=120)
 
-        # foods = Food.objects.all()
-        # food = foods[0].calories
+        foods = Food.objects.all()
+        food = foods[0]
+        self.assertEqual(len(foods), 4)
+        self.assertEqual(food.name, "kiwi")
+        self.assertEqual(food.calories, 55)
+
         response = self.client.get('/api/v1/foods/')
 
-        # print(food)
-        # print(type(food))
         self.assertEqual(len(response), 4)
